5d677ac46507be77d4d02e351f7ce522ec23061a
FrancoPagnotta <fpagnotta@mobydigital.com>
2021-12-08 00:24:40 -0300

    proyecto creado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './pages/registro/registro.component';
+
+const routes: Routes = [
+  { path: '', component: RegistroComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
